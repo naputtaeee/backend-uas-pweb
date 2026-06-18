a65f4f1376b881e6f310ed1121e90931ef63ca7b
nabilaputri <nabilaputripku1@gmail.com>
2026-06-18 16:08:42 +0700

    nambahin lg

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+ port: process.env.DB_PORT || 4000,
+  connectTimeout: 20000, 
   ssl: {
     rejectUnauthorized: true, 
     minVersion: 'TLSv1.2'    
